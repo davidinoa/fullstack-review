96ea3ce7ee62f248d9654d032461555989b4e696
David Inoa <davidinoa@gmail.com>
2018-01-03 17:51:41 -0500

    Design repos schema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  owner: String,
+  url: {
+    type: String,
+    required: true
+  },
+  description: String,
+  stars: Number,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -11,6 +26,6 @@ let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-}
+};
 
 module.exports.save = save;
